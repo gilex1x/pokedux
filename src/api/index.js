13d0c192021ffb60067dc387f387b2595d3b90af
gilberto <gilbertosa099@gmail.com>
2022-05-28 12:12:45 -0500

    [ADD]Fetch to pokeApi

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,10 @@
 const URL = process.env.URL || "https://pokeapi.co/api/v2/";
+const LIMIT = 150;
+
+export const getLimitedPokemons = () => {
+  return fetch(`${URL}pokemon?limit=${LIMIT}`);
+};
+
+export const getPokemonById =(id) => {
+  return fetch(`${URL}pokemon/${id}`);
+};
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import PokeList from '../../components/PokeList';
-import Searcher from '../../components/Searcher';
-import './styles.css';
+import React, { useEffect } from "react";
+import PokeList from "../../components/PokeList";
+import Searcher from "../../components/Searcher";
+import { getLimitedPokemons } from "../../api";
+import "./styles.css";
 
 function Home() {
+  useEffect(() => {
+    getLimitedPokemons()
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className='Home'>
       <Searcher />
